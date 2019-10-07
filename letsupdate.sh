81365df4dfd29cebb9dfb068013aa3c128f385c3
Tobias Fichtner <52255158+tobiasfichtner@users.noreply.github.com>
2019-10-07 05:09:26 +0200

    Update letsupdate.sh
    some fixes

--- letsupdate.sh
+++ letsupdate.sh
@@ -10,18 +10,21 @@
 TMP_LIST_PASSWD=( $(getent passwd `/usr/bin/id -un` | sed 's/:/ /g' ))
 USERNAME=${TMP_LIST_PASSWD[0]}
 HOMEDIR=${TMP_LIST_PASSWD[4]}
+USERIPv4=$(dig $USERNAME.$HOSTNAME A +short | grep -Eo '[0-9\.]{7,15}' | head -1)
+USERIPv6=$(dig $USERNAME.$HOSTNAME AAAA +short | grep -Eo '[0-9A-Fa-f\:]{7,45}' | head -1)
 
 # Let’s Encrypt Settings
 LETSSHARE="$HOMEDIR/.local/share/letsencrypt"
 LETSWORK="$LETSSHARE/work/"
 LETSCONFIG="$HOMEDIR/.config/letsencrypt"
 LETSLOG="$LETSSHARE/logs/"
-LETSLIVE="$LETSCONFIG/live/"
+LETSLIVE="$LETSCONFIG/live"
 LETSCHALLANGE="/var/www/virtual/$USERNAME/html/"
 LETSRSA=4096
 LETSVALIDDAYS=5
 
 ## certificate function
+#
 # order or renew a cert and add it
 function certificate ( ) {
         /usr/local/bin/letsencrypt certonly \
@@ -40,19 +43,35 @@ function certificate ( ) {
                 --post-hook "/usr/local/bin/uberspace-add-certificate -k \"$LETSLIVE/$1/privkey.pem\" -c \"$LETSLIVE/$1/cert.pem\""
 }
 
+function getIPv4 ( ) {
+        echo `dig $1 A +short | grep -Eo '[0-9\.]{7,15}' | head -1`
+}
+
+function getIPv6 ( ) {
+        echo `dig $1 AAAA +short | grep -Eo '[0-9A-Fa-f\:]{7,45}' | head -1`
+}
+
 #
 ## update loop
 #
 
-
 for domain in `/usr/local/bin/uberspace-list-domains -w | grep -v "*.$USERNAME.$HOSTNAME"`; do
-        cert="$LETSLIVE$domain/cert.pem"
-        if [ -f $cert ]; then
-                openssl x509 -checkend $(( $LETSVALIDDAYS * 86400 )) -in $cert > /dev/null
-                if [ $? != 0 ]; then
-                        certificate $domain
-                fi
+        cert="$LETSLIVE/$domain/cert.pem"
+        if [ $USERIPv4 == "$(getIPv4 $domain)" ] || [ $USERIPv6 == "$(getIPv6 $domain)" ];then
+            if [ -f $cert ]; then
+                    openssl x509 -checkend $(( $LETSVALIDDAYS * 86400 )) -in $cert > /dev/null
+                    if [ $? != 0 ]; then
+                            echo
+                            certificate $domain
+                    fi
+            else
+                    echo
+                    certificate $domain
+            fi
         else
-                certificate $domain
+            echo
+            echo "removing not linked domain: $domain"
+            /usr/local/bin/uberspace-del-domain -w -d $domain
         fi
+        wait
 done
